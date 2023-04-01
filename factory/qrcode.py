3ae889b569ae0ccaa07de1d3a3eccdacc7e14eb3
Sam <100821827+01101sam@users.noreply.github.com>
2023-04-02 01:19:24 +0800

    Update qrcode.py

--- factory/qrcode.py
+++ factory/qrcode.py
@@ -6,7 +6,7 @@ with open('../README.template.md', 'r') as file:
 qr_service_endpoint = 'https://api.qrserver.com/v1/create-qr-code/?size=300x300&data='
 content = content.replace('{{qr_service_endpoint}}', qr_service_endpoint)
 
-github_raw_content_endpoint = 'https://raw.githubusercontent.com/walkonthemarz/Shadowrocket-ADBlock-Rules/master/'
+github_raw_content_endpoint = 'https://raw.githubusercontent.com/SpeedQualityCoding/Shadowrocket-ADBlock-Rules/master/'
 content = content.replace('{{github_raw_content_endpoint}}', github_raw_content_endpoint)
 
 url_encoded_github_raw_content_endpoint = urllib.parse.quote(github_raw_content_endpoint, safe='')
